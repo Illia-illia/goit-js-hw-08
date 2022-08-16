cd863ebf430d122956cc77113ae603af67b493b1
Illia-Illia <Takoexerrorx@gmail.com>
2022-08-16 21:33:32 +0300

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,12 @@
-const player = require('@vimeo/player');
-console.log(player);
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  console.log(localStorage.getItem('videoplayer-current-time'));
+};
+
+player.on('timeupdate', onPlay);
